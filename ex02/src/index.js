dc2c815af8a757f4fddd15840961737e7d2183f7
edelhida <edelhida.dervisevic@gmail.com>
2021-03-10 20:51:55 +0100

    Task02 added

---
+++ ex02/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    // Only change code below this line 
+    // This is single line of JavaScript comment
+    /* document.write("This is a multi-line comment!");
+    document.write("This is a multi-line comment!");
+    document.write("This is a multi-line comment!");
+    document.write("This is a multi-line comment!");
+    document.write("This is a multi-line comment!");
+    */
+    // Only change code above this line 
+}
+
+console.log(main());
+
+module.exports = main;
